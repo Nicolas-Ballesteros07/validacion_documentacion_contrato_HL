1fe77108489dfafd6c33ea3a0b3e5ae634704ea5
Nicolas-Ballesteros07 <automatizacion@solutionsandpayroll.com>
2026-07-01 15:18:40 -0500

    FIX: CORRECION DE BUILDS Y VERCEL

--- app/app/wsgi.py
+++ app/app/wsgi.py
@@ -1,15 +1,9 @@
-"""
-WSGI config for app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
