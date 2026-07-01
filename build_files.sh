0bb24568ff26ce2223638eb2b5f201c04d55138f
Nicolas-Ballesteros07 <automatizacion@solutionsandpayroll.com>
2026-07-01 15:25:56 -0500

    FIX: SETTINGS

--- build_files.sh
+++ build_files.sh
@@ -6,4 +6,6 @@ python3 -m pip install -r requirements.txt
 echo "Collecting static files..."
 cd app
 python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
 cd ..
